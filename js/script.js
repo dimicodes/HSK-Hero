c8cfb309a612a88e9a71712557a124a5ea43a0d0
dimicodes <dkmiamib@gmail.com>
2023-10-26 23:08:54 +0200

    10/26

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ let doneStudyingBtn = document.getElementById("doneStudyingBtn");
 doneStudyingBtn.style.display = "none";
 doneStudyingBtn.addEventListener("click", doneStudying);
 
+let dueForReviewNumber = document.getElementById("dueForReviewNumber");
 let wordsKnownStats = document.getElementById("words-known-stats");
 let wordsMasteredStats = document.getElementById("words-mastered-stats");
 let wordsRemainingStats = document.getElementById("words-remaining-stats");
@@ -27,40 +28,91 @@ let doYouKnowText = document.getElementById("doYouKnowText");
 let sampleContainer = document.getElementById("sample-container");
 sampleContainer.style.display = "none";
 
+let pinyin = document.getElementById("pinyin");
+pinyin.style.display = "none";
+
 let englishTranslation = document.getElementById("englishTranslation");
 englishTranslation.style.display = "none";
 
 let chineseSampleSentence = document.getElementById("chineseSampleSentence");
 let chineseSampleSentenceTranslation = document.getElementById("chineseSampleSentenceTranslation");
 
-let randomWord, randomChineseWord, chineseWordTranslation, chineseSentence, englishSentence;
+let randomWord = null;
+let randomChineseWord, chineseWordTranslation, chineseSentence, englishSentence;
+
 
 // List of words that are due for review according to their "nextReviewDate" property
 let dueForReview = [];
 
 let nextWordsList = [];
 
+// Button triggers for review mode studying and new word studying
+let isReviewMode = false;
+
 // Vocabulary Lists
 let selectedVocabularyList = hsk1;
 
-// 
+let currentVocabularyList = selectedVocabularyList;
+
+
+
+// Set keys for HSK1 and HSK2 vocabulary lists in local storage
+const hsk1LocalStorageKey = "hsk1VocabList";
+const hsk2LocalStorageKey = "hsk2VocabList";
+
+// Check if HSK1 vocabulary list exists in local storage, if not, initialize it
+let hsk1VocabularyList = JSON.parse(localStorage.getItem(hsk1LocalStorageKey));
+if (!hsk1VocabularyList) {
+    hsk1VocabularyList = hsk1; // Default to hsk1 if not found
+}
+
+// Check if HSK2 vocabulary list exists in local storage, if not, initialize it
+let hsk2VocabularyList = JSON.parse(localStorage.getItem(hsk2LocalStorageKey));
+if (!hsk2VocabularyList) {
+    hsk2VocabularyList = hsk2; // Default to hsk2 if not found
+}
+
+// If hsk1 button is pressed, change selectedVocabularyList variable, clear nextWordsList and replenish list, refresh stats for new vocab list
 let hsk1Btn = document.getElementById("hsk1");
 hsk1Btn.addEventListener("click", function(){
-    changeColor(hsk1Btn);
-    selectedVocabularyList = hsk1;
+    changeHskColor(hsk1Btn);
+    selectedVocabularyList = hsk1VocabularyList;
     nextWordsList = [];
     initialWordList();
+    // Clear the "dueForReview" list when switching to HSK1
+    dueForReview = [];
+    updateDueForReviewList(selectedVocabularyList);
+
+    countStats(selectedVocabularyList);
+    // localStorage.setItem(hsk1LocalStorageKey, JSON.stringify(hsk1VocabularyList)); // Save the selected list
     console.log("hsk1 vocab list selected");
 });
 
 let hsk2Btn = document.getElementById("hsk2");
 hsk2Btn.addEventListener("click", function(){
-    changeColor(hsk2Btn);
-    selectedVocabularyList = hsk2;
+    changeHskColor(hsk2Btn);
+    selectedVocabularyList = hsk2VocabularyList;
     nextWordsList = [];
     initialWordList();
+    isReviewMode = false;
+    // Clear the "dueForReview" list when switching to HSK1
+    dueForReview = [];
+    updateDueForReviewList(selectedVocabularyList);
+
+
+    changeColor(newWordsBtn);
+    countStats(selectedVocabularyList);
+    console.log("due for review list: ", dueForReview);
+
+    // localStorage.setItem(hsk2LocalStorageKey, JSON.stringify(hsk2VocabularyList)); // Save the selected list
     console.log("hsk2 vocab list selected");
 });
+
+// Millisecond conversions and variables for testing purposes
+const halfMinInMilliseconds = 30000;
+const oneMinInMilliseconds = 60000;
+const twoMinInMilliseconds = 120000;
+
 
 // Millisecond conversions and variables for Spaced Repetition Schedule
 const halfDayInMilliseconds = 0.5 * 24 * 60 * 60 * 1000;
@@ -72,19 +124,17 @@ const thirtyDaysInMilliseconds = 30 * 24 * 60 * 60 * 1000;
 const fortyFiveDaysInMilliseconds = 45 * 24 * 60 * 60 * 1000;
 const sixtyDaysInMilliseconds = 60 * 24 * 60 * 60 * 1000;
 
-// Button triggers for review mode studying and new word studying
-let isReviewMode = false;
 
 let newWordsBtn = document.getElementById("newWords");
 newWordsBtn.addEventListener("click", function() {
-    isReviewMode = false;
     changeColor(newWordsBtn);
+    isReviewMode = false;
 });
 
 let reviewWordsBtn = document.getElementById("reviewWords");
 reviewWordsBtn.addEventListener("click", function() {
-    isReviewMode = true;
     changeColor(reviewWordsBtn);
+    isReviewMode = true;
 });
 
 
@@ -100,8 +150,22 @@ function changeColor(button) {
     button.classList.add('mode-button-clicked');
 }
 
+function changeHskColor(button) {
+    // Remove the highlighting class from all buttons
+    const buttons = document.querySelectorAll('.mode-button-hsk');
+    buttons.forEach((btn) => {
+      btn.classList.remove('mode-button-hsk-clicked');
+    });
+  
+    // Add the highlighting class to the clicked button
+    button.classList.add('mode-button-hsk-clicked');
+}
+
 
 function pushNewWordToInitialWordList(vocabularyList){
+    if (randomWord === null) {
+        randomWord = {}; // Initialize randomWord as an empty object
+    }
     // Get a random index within the range of the hsk1 vocabulary list
     let randomIndex = Math.floor(Math.random() * vocabularyList.length);
 
@@ -133,7 +197,17 @@ function initialWordList(){
 // Updates dueForReview list
 function updateDueForReviewList(vocabularyList) {
     const now = Date.now();
-    dueForReview.push(...vocabularyList.filter(word => word.nextReviewDate <= (now - halfDayInMilliseconds)));
+    dueForReview.push(
+        ...vocabularyList.filter(word => 
+            word.rightInARow > 0 && word.nextReviewDate <= now
+        )
+    );
+
+    if (dueForReview.length === 0) {
+        reviewWordsBtn.disabled = true;
+    } else {
+        reviewWordsBtn.disabled = false;
+    }
     console.log("due for review list: ", dueForReview);
 }
 
@@ -152,14 +226,19 @@ function countStats(vocabularyList){
             ++wordsRemaining;
         }
     }
+ 
+    dueForReviewStat = dueForReview.length;
+
     wordsKnownStats.innerHTML = wordsKnown;
     wordsMasteredStats.innerHTML = wordsMastered;
     wordsRemainingStats.innerHTML = wordsRemaining;
+    dueForReviewNumber.innerHTML = dueForReviewStat
 }
 
 
 // Gets the next vocab word
 function getNextWord(){
+    pinyin.style.display = "none";
     englishTranslation.style.display = "none";
     sampleContainer.style.display = "none";
 
@@ -169,12 +248,14 @@ function getNextWord(){
     if (isReviewMode && dueForReview.length >= 1){
         randomWord = dueForReview.shift();
         randomChineseWord = randomWord.chinese;
+        pinyinWord = randomWord.pinyin;
         chineseWordTranslation = randomWord.english;
         chineseSentence = randomWord.sampleChinese;
         englishSentence = randomWord.sampleEnglish;
     } else {
         randomWord = nextWordsList.shift();
         randomChineseWord = randomWord.chinese;
+        pinyinWord = randomWord.pinyin;
         chineseWordTranslation = randomWord.english;
         chineseSentence = randomWord.sampleChinese;
         englishSentence = randomWord.sampleEnglish;
@@ -194,6 +275,7 @@ function unknownWord(){
     sampleContainer.style.display = "inline-block";
     vocabWordInitial.style.display = "none";
     vocabWord.style.display = "block";
+    pinyin.style.display = "block";
     englishTranslation.style.display = "block";
     doneStudyingBtn.style.display = "block";
     unknownWordBtn.style.display = "none";
@@ -201,6 +283,7 @@ function unknownWord(){
     forgotWordText.style.display = "block";
     doYouKnowText.style.display = "none";
 
+    pinyin.innerHTML = pinyinWord;
     englishTranslation.innerHTML = chineseWordTranslation;
     chineseSampleSentence.innerHTML = chineseSentence;
     chineseSampleSentenceTranslation.innerHTML = englishSentence;
@@ -209,6 +292,13 @@ function unknownWord(){
     randomWord.rightInARow = 0;
     // puts the unknown word in queue to be reviewed again (in index position 3)
     nextWordsList.splice(3,0,randomWord);
+
+
+    // Save the selected HSK1 vocabulary list in local storage
+    localStorage.setItem(hsk1LocalStorageKey, JSON.stringify(hsk1VocabularyList));
+
+    // Save the selected HSK2 vocabulary list in local storage
+    localStorage.setItem(hsk2LocalStorageKey, JSON.stringify(hsk2VocabularyList));
 
     console.log("randomWord: ", randomWord);
     console.log("initialWordList: ", nextWordsList);
@@ -236,6 +326,16 @@ function knownWord(){
         randomWord.nextReviewDate = Date.now() + sixtyDaysInMilliseconds;
     }
 
+    if (dueForReview.length === 0){
+        isReviewMode = false;
+        changeColor(newWordsBtn);
+    }
+
+    // Save the selected HSK1 vocabulary list in local storage
+    localStorage.setItem(hsk1LocalStorageKey, JSON.stringify(hsk1VocabularyList));
+
+    // Save the selected HSK2 vocabulary list in local storage
+    localStorage.setItem(hsk2LocalStorageKey, JSON.stringify(hsk2VocabularyList));
     
     console.log(randomWord.chinese, randomWord.rightInARow);
     console.log(randomWord.nextReviewDate);
@@ -264,13 +364,10 @@ function doneStudying(){
 
 
 // Upon initial page load
+selectedVocabularyList = hsk1VocabularyList;
 initialWordList();
 countStats(selectedVocabularyList);
-updateDueForReviewList(selectedVocabularyList);
-if (dueForReview.length >= 1){
-    isReviewMode = true;
-    changeColor(reviewWordsBtn);
-}
+// updateDueForReviewList(selectedVocabularyList);
 getNextWord();
 
 
